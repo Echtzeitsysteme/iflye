526c5021ee025be820797f3e4c148fdf75378ea7
Maximilian Kratz <account@maxkratz.com>
2021-05-19 11:31:56 +0200

    Path name is generated by node IDs in model

--- test.suite/src/test/model/ModelFacadeEmbeddingTest.java
+++ test.suite/src/test/model/ModelFacadeEmbeddingTest.java
@@ -144,9 +144,11 @@ public class ModelFacadeEmbeddingTest {
     ModelFacade.getInstance().addLinkToNetwork("l8", "virt", 12, "srv6", "srv5");
     ModelFacade.getInstance().createAllPathsForNetwork("sub");
 
-    ModelFacade.getInstance().embedLinkToPath("0", "l7");
+    final String pathName = "path-srv1-srv2";
 
-    final SubstratePath subPath = (SubstratePath) ModelFacade.getInstance().getPathById("0");
+    ModelFacade.getInstance().embedLinkToPath(pathName, "l7");
+
+    final SubstratePath subPath = (SubstratePath) ModelFacade.getInstance().getPathById(pathName);
     assertEquals(1, subPath.getGuestLinks().size());
     assertEquals("l7", subPath.getGuestLinks().get(0).getName());
 
