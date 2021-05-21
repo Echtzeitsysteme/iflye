6d2f0a516e3a7e120a0804370eedbf85cd1b0db9
Maximilian Kratz <account@maxkratz.com>
2021-05-21 10:09:33 +0200

    Extended VNE PM algorithm tests

--- test.suite/src/test/algorithms/pm/VnePmMdvneAlgorithmTest.java
+++ test.suite/src/test/algorithms/pm/VnePmMdvneAlgorithmTest.java
@@ -1,6 +1,9 @@
 package test.algorithms.pm;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import algorithms.pm.VnePmMdvneAlgorithm;
 import model.SubstrateNetwork;
 import model.VirtualNetwork;
@@ -16,6 +19,44 @@ public class VnePmMdvneAlgorithmTest extends AAlgorithmMultipleVnsTest {
   @Override
   public void initAlgo(final SubstrateNetwork sNet, final Set<VirtualNetwork> vNets) {
     algo = VnePmMdvneAlgorithm.prepare(sNet, vNets);
+  }
+
+  @AfterEach
+  public void resetAlgo() {
+    if (algo != null) {
+      ((VnePmMdvneAlgorithm) algo).dispose();
+    }
+  }
+
+  @Test
+  public void testMultipleNetworksAfterEachOther() {
+    twoTierSetupFourServers("sub", 3);
+    final SubstrateNetwork sNet = (SubstrateNetwork) facade.getNetworkById("sub");
+
+    for (int i = 0; i < 2; i++) {
+      final String currVnetId = "virt" + i;
+      facade.addNetworkToRoot(currVnetId, true);
+      oneTierSetupTwoServers(currVnetId, 1);
+
+      final VirtualNetwork currVnet = (VirtualNetwork) facade.getNetworkById(currVnetId);
+      initAlgo(sNet, Set.of(currVnet));
+      assertTrue(algo.execute());
+
+      checkAllElementsEmbeddedOnSubstrateNetwork(sNet, Set.of(currVnet));
+    }
+  }
+
+  @Test
+  public void testMaxSizeVnet() {
+    oneTierSetupTwoServers("sub", 2);
+    final SubstrateNetwork sNet = (SubstrateNetwork) facade.getNetworkById("sub");
+    oneTierSetupTwoServers("virt", 2);
+    final VirtualNetwork currVnet = (VirtualNetwork) facade.getNetworkById("virt");
+
+    initAlgo(sNet, Set.of(currVnet));
+    assertTrue(algo.execute());
+
+    checkAllElementsEmbeddedOnSubstrateNetwork(sNet, Set.of(currVnet));
   }
 
 }
