450f879ed5f79007ea0bffb5f151df068f599207
Maximilian Kratz <account@maxkratz.com>
2021-06-16 17:18:04 +0200

    Adapted rendering script

--- scripts/rendering/render.sh
+++ scripts/rendering/render.sh
@@ -3,10 +3,27 @@
 set -e
 
 # Find all files with suffix .csv
-BASE_PATH="../metrics"
-cp $BASE_PATH/*.csv .
+if [ $1 = "iflye" ]; then
+    BASE_PATH="../metrics"
+    cp $BASE_PATH/*.csv .
 
-FILES=$(find . -name '*.csv')
+    FILES=$(find . -name '*.csv')
+elif [ $1 = "idyve" ]; then
+    BASE_PATH="../idyve-output"
+    PATHS=$(find $BASE_PATH -name 'metrics.csv')
+    for p in $PATHS
+    do
+        current_p=$(echo $p | awk -F / '{ print $(NF-1) }')
+        mkdir -p $current_p
+        cp $p ./$current_p/metrics.csv
+    done
+
+    FILES=$(find . -name '*.csv')
+else
+    echo "First parameter was invalid."
+    exit 1
+fi
+
 for f in $FILES
 do
     # Set filename to latex env, this is needed by the tex template
