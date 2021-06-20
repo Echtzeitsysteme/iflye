31876be73c525911d6712a9133052e8f33c46093
Maximilian Kratz <account@maxkratz.com>
2021-06-20 17:56:53 +0200

    Added argument eval scripts for batch job runner

---
+++ scripts/start-args-idyve.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+function setup {
+    # Extract hipe-network.xmi file
+    unzip -o $JAR "*/ibex-patterns.xmi"
+    rsync -a ./src-gen/* ./
+    rm -r ./src-gen
+
+    mkdir -p logs
+}
+
+function run {
+    # Execute the program itself and save its output to logfile
+    java -Xmx32g -jar $JAR $ARGS 2>&1 | tee "./logs/$RUN_NAME.log"
+}
+
+# Set env vars
+source env.sh
+
+# Config
+export JAR="idyve.jar"
+export OUTPUT="idyve-output/"
+
+setup
+
+# Example arguments:
+# mdvne two-tier 4 12
+# $1    $2       $3 $4
+
+export a=$1
+export s=$2
+export l=$3
+export p=$4
+
+export RUN_NAME="${a}_${s}-${p}-pods_l${l}_k1"
+mkdir -p $OUTPUT$RUN_NAME
+export ARGS="$a $p $s $l $OUTPUT$RUN_NAME/ 600 0 NEVER"
+echo "#"
+echo "# => Using ARGS: $ARGS"
+echo "#"
+run
+
+echo "#"
+echo "# => Arg script done."
+echo "#"
---
+++ scripts/start-args.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+function setup {
+    # Make sure that folder for hipe-network exists
+    mkdir -p bin
+    mkdir -p metrics
+    mkdir -p resources
+
+    # Get resources from scenario project
+    if ! [[ "$(find ./resources -maxdepth 3 -type f -iname \*.json)" ]];
+    then 
+        rsync -a ../vne.scenarios/resources .
+    fi
+
+    # Extract hipe-network.xmi file
+    unzip -o $JAR "*/hipe-network.xmi"
+    rsync -a ./rules ./bin
+    rm -r ./rules
+
+    mkdir -p logs
+}
+
+function run {
+    # Execute the program itself and save its output to logfile
+    java -Xmx32g -jar $JAR $ARGS 2>&1 | tee "./logs/$RUN_NAME.log"
+}
+
+# Set env vars
+source env.sh
+
+# Config
+export JAR="iflye.jar"
+
+setup
+
+# Example arguments:
+# pm two-tier-4-pods 4 1
+# $1 $2              $3 $4
+
+export a=$1
+export s=$2
+export l=$3
+export k=$4
+
+export RUN_NAME="${a}_${s}_l${l}_k${k}"
+export ARGS="-a $a -o total-comm-c -e emoflon_wo_update -l $l -k $k -s resources/$s/snet.json -v resources/$s/vnets.json -c ./metrics/$RUN_NAME.csv -i 600"
+echo "#"
+echo "# => Using ARGS: $ARGS"
+echo "#"
+run
+
+echo "#"
+echo "# => Arg script done."
+echo "#"
