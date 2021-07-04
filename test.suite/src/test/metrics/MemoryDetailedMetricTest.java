01448c7451c1e26e6c39fc25498e4bb4b4f18213
Maximilian Kratz <account@maxkratz.com>
2021-07-04 12:24:15 +0200

    Added test class templates

---
+++ test.suite/src/test/metrics/MemoryDetailedMetricTest.java
@@ -0,0 +1,15 @@
+package test.metrics;
+
+import org.junit.jupiter.api.Disabled;
+
+/**
+ * Test class for the detailed metric of used memory.
+ * 
+ * @author Maximilian Kratz {@literal <maximilian.kratz@stud.tu-darmstadt.de>}
+ */
+@Disabled
+public class MemoryDetailedMetricTest extends AMetricTest {
+
+  // TODO
+
+}
