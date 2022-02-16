79966b0ab44a5c6499a7da5be56525a558f405af
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-02-16 14:39:30 +0100

    Adds extended variant which creates nice HTML reports
    Cleanup

--- test.suite/src/test/GlobalTestRunner.java
+++ test.suite/src/test/GlobalTestRunner.java
@@ -14,9 +14,11 @@ import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import org.junit.platform.reporting.legacy.xml.LegacyXmlReportGeneratingListener;
 
+import io.github.gdiegel.junit5_html_report_generator.ExtentReportGeneratingListener;
+
 /**
  * Global test runner class that can be used to trigger test execution, e.g.,
- * from CLI.
+ * from CLI or within a CI.
  * 
  * @author Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
  */
@@ -28,25 +30,30 @@ public class GlobalTestRunner {
 	 * @param args Parameters will be ignored.
 	 */
 	public static void main(final String[] args) {
+		// Generate all listeners
 		final SummaryGeneratingListener listener = new SummaryGeneratingListener();
 		final LegacyXmlReportGeneratingListener xmlListener = new LegacyXmlReportGeneratingListener(
 				Path.of("./build/reports"), new PrintWriter(System.out));
+		final ExtentReportGeneratingListener extentReportGeneratingListener = new ExtentReportGeneratingListener();
 
+		// Find tests and build request
 		final LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request() //
-//				.selectors(selectPackage("test.algorithms.heuristics")) //
 				.selectors(selectPackage("test")) //
 				.filters(includeClassNamePatterns(".*Test")) //
 				.build();
 
+		// Build launcher and register all listeners
 		final Launcher launcher = LauncherFactory.create();
 		launcher.registerTestExecutionListeners(listener);
 		launcher.registerTestExecutionListeners(xmlListener);
+		launcher.registerTestExecutionListeners(extentReportGeneratingListener);
 
+		// Run tests
 		launcher.execute(request);
 
+		// Print summary to console
 		final TestExecutionSummary summary = listener.getSummary();
 		summary.printTo(new PrintWriter(System.out));
-
 		System.exit(0);
 	}
 
