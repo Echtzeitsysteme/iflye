cd1eedb47d036b48ecfc80a5be16db2212d0697b
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-02-16 14:21:25 +0100

    Adds first variant for programmatic test runner

---
+++ test.suite/src/test/GlobalTestRunner.java
@@ -0,0 +1,53 @@
+package test;
+
+import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+
+import java.io.PrintWriter;
+import java.nio.file.Path;
+
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.junit.platform.reporting.legacy.xml.LegacyXmlReportGeneratingListener;
+
+/**
+ * Global test runner class that can be used to trigger test execution, e.g.,
+ * from CLI.
+ * 
+ * @author Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
+ */
+public class GlobalTestRunner {
+
+	/**
+	 * Main method to start the execution.
+	 * 
+	 * @param args Parameters will be ignored.
+	 */
+	public static void main(final String[] args) {
+		final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+		final LegacyXmlReportGeneratingListener xmlListener = new LegacyXmlReportGeneratingListener(
+				Path.of("./build/reports"), new PrintWriter(System.out));
+
+		final LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request() //
+//				.selectors(selectPackage("test.algorithms.heuristics")) //
+				.selectors(selectPackage("test")) //
+				.filters(includeClassNamePatterns(".*Test")) //
+				.build();
+
+		final Launcher launcher = LauncherFactory.create();
+		launcher.registerTestExecutionListeners(listener);
+		launcher.registerTestExecutionListeners(xmlListener);
+
+		launcher.execute(request);
+
+		final TestExecutionSummary summary = listener.getSummary();
+		summary.printTo(new PrintWriter(System.out));
+
+		System.exit(0);
+	}
+
+}
