ac0b1624965c79745ea50d83f4c146da1feec29e
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-03-22 10:01:22 +0100

    Adds first implementation draft of the Roam-based VNE algo + example

---
+++ examples/src/examples/algorithms/VneRoamAlgorithmExampleSmall.java
@@ -0,0 +1,77 @@
+package examples.algorithms;
+
+import java.util.Set;
+
+import algorithms.AbstractAlgorithm;
+import algorithms.roam.VneRoamAlgorithm;
+import facade.ModelFacade;
+import facade.config.ModelFacadeConfig;
+import generators.OneTierNetworkGenerator;
+import generators.TwoTierNetworkGenerator;
+import generators.config.OneTierConfig;
+import generators.config.TwoTierConfig;
+import metrics.manager.GlobalMetricsManager;
+import model.SubstrateNetwork;
+import model.VirtualNetwork;
+
+/**
+ * Runnable example for the VNE Roam algorithm implementation.
+ *
+ * @author Maximilian Kratz {@literal <maximilian.kratz@es.tu-darmstadt.de>}
+ */
+public class VneRoamAlgorithmExampleSmall {
+
+	/**
+	 * Main method to start the example. String array of arguments will be ignored.
+	 *
+	 * @param args Will be ignored.
+	 */
+	public static void main(final String[] args) {
+		// Setup
+		ModelFacadeConfig.MIN_PATH_LENGTH = 1;
+		ModelFacadeConfig.MAX_PATH_LENGTH = 4;
+
+		GlobalMetricsManager.startRuntime();
+
+		// Substrate network = two tier network
+		final OneTierConfig rackConfig = new OneTierConfig(2, 1, false, 10, 10, 10, 10);
+		final TwoTierConfig substrateConfig = new TwoTierConfig();
+		substrateConfig.setRack(rackConfig);
+		substrateConfig.setCoreBandwidth(100);
+		substrateConfig.setNumberOfCoreSwitches(1);
+		substrateConfig.setNumberOfRacks(2);
+		final TwoTierNetworkGenerator subGen = new TwoTierNetworkGenerator(substrateConfig);
+		subGen.createNetwork("sub", false);
+
+		// Virtual network = one tier network
+		final OneTierConfig virtualConfig = new OneTierConfig(4, 1, false, 10, 1, 1, 1);
+		final OneTierNetworkGenerator virtGen = new OneTierNetworkGenerator(virtualConfig);
+		virtGen.createNetwork("virt", true);
+
+		final SubstrateNetwork sNet = (SubstrateNetwork) ModelFacade.getInstance().getNetworkById("sub");
+		final VirtualNetwork vNet = (VirtualNetwork) ModelFacade.getInstance().getNetworkById("virt");
+
+		// Create and execute algorithm
+		final AbstractAlgorithm algo = VneRoamAlgorithm.prepare(sNet, Set.of(vNet));
+		algo.execute();
+
+		GlobalMetricsManager.stopRuntime();
+
+		// Save model to file
+		ModelFacade.getInstance().persistModel();
+		System.out.println("=> Execution finished.");
+
+		// Time measurements
+		System.out.println("=> Elapsed time (total): " + GlobalMetricsManager.getRuntime().getValue() / 1_000_000_000
+				+ " seconds");
+		System.out.println(
+				"=> Elapsed time (PM): " + GlobalMetricsManager.getRuntime().getPmValue() / 1_000_000_000 + " seconds");
+		System.out.println("=> Elapsed time (ILP): " + GlobalMetricsManager.getRuntime().getIlpValue() / 1_000_000_000
+				+ " seconds");
+		System.out.println("=> Elapsed time (rest): " + GlobalMetricsManager.getRuntime().getRestValue() / 1_000_000_000
+				+ " seconds");
+
+		System.exit(0);
+	}
+
+}
---
+++ vne.algorithms/src/algorithms/roam/VneRoamAlgorithm.java
@@ -0,0 +1,83 @@
+package algorithms.roam;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.emoflon.roam.roamslang.examples.mdvne.MdvneRoamIflyeAdapter;
+
+import algorithms.AbstractAlgorithm;
+import facade.ModelFacade;
+import model.SubstrateNetwork;
+import model.VirtualNetwork;
+
+/**
+ * Roam-based VNE algorithm implementation.
+ *
+ * @author Maximilian Kratz {@literal <maximilian.kratz@es.tu-darmstadt.de>}
+ */
+public class VneRoamAlgorithm extends AbstractAlgorithm {
+
+	/**
+	 * Algorithm instance (singleton).
+	 */
+	private static VneRoamAlgorithm instance;
+
+	/**
+	 * Default model saving path. Must be used for Roam to load the model.
+	 */
+	final private static String MODEL_FILE_PATH = "model-roam-algo-in.xmi";
+
+	/**
+	 * Constructor that gets the substrate as well as the virtual network.
+	 *
+	 * @param sNet  Substrate network to work with.
+	 * @param vNets Set of virtual networks to work with.
+	 */
+	public VneRoamAlgorithm(final SubstrateNetwork sNet, final Set<VirtualNetwork> vNets) {
+		super(sNet, vNets);
+	}
+
+	@Override
+	public boolean execute() {
+		// TODO: Time measurement
+		ModelFacade.getInstance().persistModel(MODEL_FILE_PATH);
+		return MdvneRoamIflyeAdapter.execute(MODEL_FILE_PATH);
+	}
+
+	/**
+	 * Initializes a new instance of the Roam-based VNE algorithm.
+	 *
+	 * @param sNet  Substrate network to work with.
+	 * @param vNets Set of virtual networks to work with.
+	 * @return Instance of this algorithm implementation.
+	 */
+	public static VneRoamAlgorithm prepare(final SubstrateNetwork sNet, final Set<VirtualNetwork> vNets) {
+		if (sNet == null || vNets == null) {
+			throw new IllegalArgumentException("One of the provided network objects was null.");
+		}
+
+		if (vNets.size() == 0) {
+			throw new IllegalArgumentException("Provided set of virtual networks was empty.");
+		}
+
+		if (instance == null) {
+			instance = new VneRoamAlgorithm(sNet, vNets);
+		}
+		instance.sNet = sNet;
+		instance.vNets = new HashSet<>();
+		instance.vNets.addAll(vNets);
+
+		return instance;
+	}
+
+	/**
+	 * Resets the algorithm instance.
+	 */
+	public void dispose() {
+		if (instance == null) {
+			return;
+		}
+		instance = null;
+	}
+
+}
