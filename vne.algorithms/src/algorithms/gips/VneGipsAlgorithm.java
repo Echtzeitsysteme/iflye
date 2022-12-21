694ae8975c508ada94d01a0b230b67d7b0202fd6
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-12-21 10:01:41 +0100

    Use resource sets in the migration algorithm + clean up
    Codestyle

--- vne.algorithms/src/algorithms/gips/VneGipsAlgorithm.java
+++ vne.algorithms/src/algorithms/gips/VneGipsAlgorithm.java
@@ -1,6 +1,5 @@
 package algorithms.gips;
 
-import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,11 +24,6 @@ public class VneGipsAlgorithm extends AbstractAlgorithm {
 	 * Algorithm instance (singleton).
 	 */
 	private static VneGipsAlgorithm instance;
-
-	/**
-	 * Default model saving path. Must be used for GIPS to load the model.
-	 */
-	final public static String MODEL_FILE_PATH = "model-gips-algo-in.xmi";
 
 	/**
 	 * Constructor that gets the substrate as well as the virtual network.
@@ -89,9 +83,6 @@ public class VneGipsAlgorithm extends AbstractAlgorithm {
 			return;
 		}
 		instance = null;
-
-		final File out = new File(VneGipsAlgorithm.MODEL_FILE_PATH);
-		out.delete();
 	}
 
 }
