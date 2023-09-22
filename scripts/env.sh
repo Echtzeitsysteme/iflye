beef3cc4afbce12a75645045ae490670fde10105
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-09-22 09:51:56 +0200

    Updates Gurobi ILP solver to 10.0.3

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,8 @@
 # Environment variables/config for the program
 export GRB_LICENSE_FILE="/home/maxkratz/gurobi.lic"
-export GUROBI_HOME="/opt/gurobi1002/linux64/"
-export LD_LIBRARY_PATH="/opt/gurobi1002/linux64/lib/"
-PATH=$PATH:~/opt/gurobi1002/linux64/bin/:~/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/
+export GUROBI_HOME="/opt/gurobi1003/linux64/"
+export LD_LIBRARY_PATH="/opt/gurobi1003/linux64/lib/"
+PATH=$PATH:~/opt/gurobi1003/linux64/bin/:~/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/
 
 # Legacy ENVs used by Cardygan-ILP
 #export GUROBI_JAR_PATH="/opt/gurobi811/linux64/lib/gurobi.jar"
