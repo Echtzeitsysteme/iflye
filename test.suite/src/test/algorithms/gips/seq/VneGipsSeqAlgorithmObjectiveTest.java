c2ed35d45a4241dc9ef24916a5d115151ebcb3e0
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-11-24 09:05:42 +0100

    Adds all tests of the normal GIPS-based impl to the sequence impl
    Clean up

---
+++ test.suite/src/test/algorithms/gips/seq/VneGipsSeqAlgorithmObjectiveTest.java
@@ -0,0 +1,63 @@
+package test.algorithms.gips.seq;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
+
+import algorithms.AlgorithmConfig;
+import algorithms.AlgorithmConfig.Objective;
+import algorithms.gips.VneGipsSeqAlgorithm;
+import model.SubstrateNetwork;
+import model.VirtualNetwork;
+import test.algorithms.gips.VneGipsAlgorithmObjectiveTest;
+
+/**
+ * Test class for the VNE GIPS sequence algorithm implementation for minimizing
+ * the total communication cost objective C.
+ *
+ * @author Maximilian Kratz {@literal <maximilian.kratz@es.tu-darmstadt.de>}
+ */
+public class VneGipsSeqAlgorithmObjectiveTest extends VneGipsAlgorithmObjectiveTest {
+
+	/**
+	 * Substrate network.
+	 */
+	SubstrateNetwork sNet;
+
+	/**
+	 * Virtual network.
+	 */
+	VirtualNetwork vNet;
+
+	@Override
+	public void initAlgo(final SubstrateNetwork sNet, final Set<VirtualNetwork> vNets) {
+		// The algorithm is only able to use the total communication objective C because
+		// it is hard-coded in GIPSL
+		AlgorithmConfig.obj = Objective.TOTAL_COMMUNICATION_OBJECTIVE_C;
+		algo = VneGipsSeqAlgorithm.prepare(sNet, vNets);
+	}
+
+	@AfterEach
+	public void resetAlgo() {
+		facade.resetAll();
+		((VneGipsSeqAlgorithm) algo).dispose();
+	}
+	
+	//
+	// Tests
+	//
+	
+	@Disabled
+	public void testAllOnOneRackLarge() {
+	}
+	
+	@Disabled
+	public void testAllOnOneServerLarge() {
+	}
+	
+	@Disabled
+	public void testAllOnMultipleRacksLarge() {
+	}
+
+}
