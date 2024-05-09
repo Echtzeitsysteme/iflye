d3d100b8118fafbddf2c8fe3f6d9bd87abc22afa
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2024-05-09 20:30:30 +0200

    Adds the Windows batch file variant of the gips-to-iflye script

---
+++ scripts/gips-ex-to-iflye.bat
@@ -0,0 +1,17 @@
+:: This script copies the generated runtime components from the
+:: (external) GIPS MdVNE example folder into this repository.
+
+setlocal
+
+set MDVNE_PROJECT_NAME=org.emoflon.gips.gipsl.examples.mdvne
+
+rmdir ..\%MDVNE_PROJECT_NAME%\src-gen /s /q
+xcopy ..\..\gips-examples\%MDVNE_PROJECT_NAME%\src-gen ..\%MDVNE_PROJECT_NAME%\src-gen\ /e
+
+rmdir ..\%MDVNE_PROJECT_NAME%.migration\src-gen /s /q
+xcopy ..\..\gips-examples\%MDVNE_PROJECT_NAME%.migration\src-gen ..\%MDVNE_PROJECT_NAME%.migration\src-gen\ /e
+
+rmdir ..\%MDVNE_PROJECT_NAME%.seq\src-gen /s /q
+xcopy ..\..\gips-examples\%MDVNE_PROJECT_NAME%.seq\src-gen ..\%MDVNE_PROJECT_NAME%.seq\src-gen\ /e
+
+endlocal
